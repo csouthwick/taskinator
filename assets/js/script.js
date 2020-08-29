a5fae181a9152fa6f5c825b9190080aed79cea2e
Chris Southwick <csouthwick@live.com>
2020-08-29 15:15:10 -0400

    load previously saved tasks on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -308,6 +308,53 @@ var saveTasks = function () {
 
 
 
+var loadTasks = function () {
+  // get tasks from localStorage
+  tasks = localStorage.getItem("tasks");
+
+  // if tasks is null, reset to an empty array and exit
+  if (!tasks) {
+    tasks = [];
+    return false;
+  }
+
+  // else parse JSON to get the data in a usable form
+  tasks = JSON.parse(tasks);
+
+  // iterate through array and create task elements on the page
+  for (let i = 0; i < tasks.length; i++) {
+    tasks[i].id = taskIdCounter;
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.setAttribute("data-task-id", tasks[i].id);
+    listItemEl.setAttribute("draggable", "true");
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    var taskActionsEl = createTaskActions(tasks[i].id);
+    listItemEl.appendChild(taskActionsEl);
+
+    if (tasks[i].status === "to do") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+      tasksToDoEl.appendChild(listItemEl);
+    } else if (tasks[i].status === "in progress") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+      tasksInProgressEl.appendChild(listItemEl);
+    } else if (tasks[i].status === "complete") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+      tasksCompletedEl.appendChild(listItemEl);
+    }
+    taskIdCounter++;
+  }
+}
+
+
+
 // Add event listeners to the page
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -316,3 +363,6 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
+
+// load any previously saved tasks
+loadTasks();
